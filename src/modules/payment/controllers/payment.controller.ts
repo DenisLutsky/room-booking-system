9089a13db9267ac4af25aa30517cb7dac0b35a0f
Denis <denis.lutsky@outlook.com>
2024-03-25 17:17:13 +0200

    removed unused code

--- src/modules/payment/controllers/payment.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { PaymentService } from '../services/payment.service';
-
-@Controller('payment')
-export class PaymentController {
-  public constructor(private readonly paymentService: PaymentService) {}
-}
--- src/modules/payment/payment.module.ts
+++ src/modules/payment/payment.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 import { InvoiceEntity } from './entities';
-import { PaymentController } from './controllers';
 import { PaymentService } from './services';
 import { InvoicesRepository } from './repository';
 
 @Module({
   imports: [MikroOrmModule.forFeature([InvoiceEntity])],
-  controllers: [PaymentController],
   providers: [PaymentService, InvoicesRepository],
   exports: [PaymentService],
 })
